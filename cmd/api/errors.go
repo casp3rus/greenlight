561fcaffab204e68951e211fd999b62e64ddb5c9
Adrian M. Popan <adrian.popan@gmail.com>
2023-02-27 19:15:37 -0600

    Create editConflictResponse app method

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -43,3 +43,8 @@ func (app *appllication) badRequestResponse(w http.ResponseWriter, r *http.Reque
 func (app *appllication) failedValidationResponse(w http.ResponseWriter, r *http.Request, errors map[string]string) {
 	app.errorResponse(w, r, http.StatusUnprocessableEntity, errors)
 }
+
+func (app *appllication) editConflictResponse(w http.ResponseWriter, r *http.Request) {
+	message := "unable to update the record due to an edit conflict, please try again"
+	app.errorResponse(w, r, http.StatusConflict, message)
+}
