58d47fb9acdcd07a29f970be33da073713cfa2ce
Adrian M. Popan <adrian.popan@gmail.com>
2023-02-27 23:54:18 -0600

    Add GetAll method on the MockMovieModel to satisfy the Movies interface.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -212,11 +212,17 @@ func (m MockMovieModel) Get(id int64) (*Movie, error) {
 	// Mock the action ...
 	return nil, nil
 }
+
 func (m MockMovieModel) Update(movie *Movie) error {
 	// Mock the action ...
 	return nil
 }
+
 func (m MockMovieModel) Delete(id int64) error {
 	// Mock the action ...
 	return nil
+}
+
+func (m MockMovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
+	return nil, nil
 }
