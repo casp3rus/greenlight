870048f7d5d1e00a76d59074abec0ade4049f5b1
Adrian M. Popan <adrian.popan@gmail.com>
2023-03-07 17:23:11 -0600

    move the code related to http.Server out of the main() into it's own file.

---
+++ cmd/api/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func (app *appllication) serve() error {
+	// Declare the HTTP server
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// Log a "starting server" message.
+	app.logger.PrintInfo("starting server", map[string]string{
+		"addr": srv.Addr,
+		"env":  app.config.env,
+	})
+
+	// Start the server
+	return srv.ListenAndServe()
+}
