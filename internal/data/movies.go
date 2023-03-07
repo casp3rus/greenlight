3dd4e68c02ad364eaa1c2fb43fb1d9efdd726601
Adrian M. Popan <adrian.popan@gmail.com>
2023-03-06 20:38:10 -0600

    Update to MockMovie struct method to return the Metadata.

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -207,6 +207,7 @@ func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*M
 	}
 
 	metadata := calculateMetadata(totalRecords, filters.Page, filters.PageSize)
+
 	return movies, metadata, nil
 }
 
@@ -232,6 +233,6 @@ func (m MockMovieModel) Delete(id int64) error {
 	return nil
 }
 
-func (m MockMovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	return nil, nil
+func (m MockMovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error) {
+	return nil, Metadata{}, nil
 }
