89930a91302cce8d043530e80028e976fd01a7ac
Adrian M. Popan <adrian.popan@gmail.com>
2023-03-12 16:35:47 -0500

    Add logic to setup the PermissionModel

---
+++ internal/data/permissions.go
@@ -0,0 +1,63 @@
+package data
+
+import (
+	"context"
+	"database/sql"
+	"time"
+)
+
+// Permissions slice used to hold the permissions codes for a single user.
+type Permissions []string
+
+// Include() method checks wether the Permissions slice contains a specific permission code.
+func (p Permissions) Include(code string) bool {
+	for i := range p {
+		if code == p[i] {
+			return true
+		}
+	}
+	return false
+}
+
+// Define the PermissonsModel type.
+type PermissonModel struct {
+	DB *sql.DB
+}
+
+// The GetAllForUser() method returns all permission codes for a user in a Permissions slice.
+func (m PermissonModel) GetAllForUser(userID int64) (Permissions, error) {
+	query := `
+	SELECT permissions.code
+	FROM permissions
+	INNER JOIN users_permissions ON users_permissions.permission_id = permissions.id
+	INNER JOIN users ON users_permissions.user_id = user.id
+	WHERE users.id = $1`
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	rows, err := m.DB.QueryContext(ctx, query, userID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var permissions Permissions
+
+	for rows.Next() {
+		var permission string
+
+		err := rows.Scan(&permission)
+		if err != nil {
+			return nil, err
+		}
+
+		permissions = append(permissions, permission)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return permissions, nil
+}
