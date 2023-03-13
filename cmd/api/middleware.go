d842f5bde84dc6ed0b214a38de96061ca6430cfd
Adrian M. Popan <adrian.popan@gmail.com>
2023-03-12 20:14:08 -0500

    Add logic to always set the header to Vary: Origin if there is no case sensitive, exact match in the trusted origins matching the request Origin header.

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -181,7 +181,18 @@ func (app *appllication) requirePermission(code string, next http.HandlerFunc) h
 
 func (app *appllication) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Vary", "Origin")
+
+		origin := r.Header.Get("Origin")
+
+		if origin != "" {
+			for i := range app.config.cors.trustedOrigins {
+				if origin == app.config.cors.trustedOrigins[i] {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+					break
+				}
+			}
+		}
 
 		next.ServeHTTP(w, r)
 	})
