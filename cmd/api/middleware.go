35bf4a719caeaa7b5576e7f3be935bae32e5a4f2
Adrian M. Popan <adrian.popan@gmail.com>
2023-03-12 19:48:34 -0500

    Added the enableCORS() middleware.

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -178,3 +178,11 @@ func (app *appllication) requirePermission(code string, next http.HandlerFunc) h
 
 	return app.requireActivatedUser(fn)
 }
+
+func (app *appllication) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		next.ServeHTTP(w, r)
+	})
+}
