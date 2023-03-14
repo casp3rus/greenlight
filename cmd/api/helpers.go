2a0494e593955ebfd00721791e68a90ed2fc0efa
Adrian M. Popan <adrian.popan@gmail.com>
2023-03-14 15:08:13 -0500

    Formating the files

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -139,7 +139,7 @@ func (app *appllication) background(fn func()) {
 	app.wg.Add(1)
 	go func() {
 		defer app.wg.Done()
-		
+
 		defer func() {
 			if err := recover(); err != nil {
 				app.logger.PrintError(fmt.Errorf("%s", err), nil)
