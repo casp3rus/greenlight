f17785de7fbb3446cfef61cc48333a08b940f328
Adrian M. Popan <adrian.popan@gmail.com>
2023-03-16 21:06:02 -0500

    Add logic for createActivationTokenHandler.

--- cmd/api/tokens.go
+++ cmd/api/tokens.go
@@ -144,3 +144,71 @@ func (app *appllication) createPasswordResetTokenHandler(w http.ResponseWriter, 
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *appllication) createActivationTokenHandler(w http.ResponseWriter, r *http.Request) {
+	// Parse and validate the user's email address.
+	var input struct {
+		Email string `json:"email"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	if data.ValidateEmail(v, input.Email); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Retrieve the corresponding user record for the email address.
+	user, err := app.models.Users.GetByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			v.AddError("email", "no matching email address found")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Return an error if the user has already been activated.
+	if user.Activated {
+		v.AddError("email", "user has been activated")
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	// Create an new activation token if user is not activated.
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ScopeActivation)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	// Email the user with their additional activation token.
+	app.background(func() {
+		data := map[string]any{
+			"activationToken": token.Plaintext,
+		}
+
+		// Use the email address stored in the DB to send the activation token.
+		err = app.mailer.Send(user.Email, "token_activation.tmpl", data)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+		}
+	})
+
+	// Send a 202 Accepted respoonse and confirmation message to the client.
+	env := envelope{"message": "an email will be sent to you containing activation instructions"}
+
+	err = app.writeJSON(w, http.StatusAccepted, env, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
